3d34f57c5929f43fe77c23bcceb005295e93ffdc
Varinolav <64284962+Varinolav@users.noreply.github.com>
2024-01-26 19:27:12 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+let tg = window.Telegram.WebApp;
+tg.expand();
+
+tg.MainButton.setText('Покормить моржика рыбкой');
+tg.MainButton.show();
+
+let korm = document.getElementById('korm');
+
+korm.addEventListener('click', (event) => {
+    document.getElementById('img').src = 'wrs2.png';
+
+
+});
